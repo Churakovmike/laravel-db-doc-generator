b90cc217ddf0ca45bb11c816ab749f5356f883e4
ChurakovMike <churakovmike@mail.ru>
2020-04-29 15:12:00 +0700

    + init repo

---
+++ src/DbDocumentorServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ChurakovMike\DbDocumentor;
+
+use Illuminate\Support\ServiceProvider;
+
+class DbDocumentorServiceProvider extends ServiceProvider
+{
+    /**
+     * Service provider for grid.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
