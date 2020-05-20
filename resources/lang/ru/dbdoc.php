6f13ec165b58f17eb18194c7fbcdcd700069b411
ChurakovMike <churakovmike@mail.ru>
2020-05-20 18:01:30 +0700

    + add russian language

---
+++ resources/lang/ru/dbdoc.php
@@ -0,0 +1,23 @@
+<?php
+return [
+    'title' => 'DbDocumentator',
+    'nav-link' => 'DbDocumentator',
+    /* ===============================  */
+    'table-header' => 'Колонки таблицы',
+    'table-column' => 'Колонка',
+    'table-column-type' => 'Тип',
+    'table-column-comment' => 'Комментарий',
+    'table-column-default' => 'По умолчанию',
+    'table-column-not-null' => 'Не null',
+    'table-column-options' => 'Дополнительно',
+    /* ===============================  */
+    'table-foreign-keys' => 'Внешние ключи',
+    'table-foreign-keys-name' => 'Имя ключа',
+    'table-foreign-keys-field' => 'Поле',
+    'table-foreign-keys-ref-table' => 'Связанная таблица',
+    'table-foreign-keys-ref-field' => 'Связанное поле',
+    /* ===============================  */
+    'table-indexes-title' => 'Индексы',
+    'table-indexes-name' => 'Название индекса',
+    'table-indexes-fields' => 'Поля',
+];
